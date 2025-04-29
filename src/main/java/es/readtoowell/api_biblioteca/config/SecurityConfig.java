5a57efa9db022bbb6a37c51a550ec2268059f16d
Héctor <UO277303@uniovi.es>
2025-04-29 20:59:14 +0200

    Modificación de la configuración del CORS

--- src/main/java/es/readtoowell/api_biblioteca/config/SecurityConfig.java
+++ src/main/java/es/readtoowell/api_biblioteca/config/SecurityConfig.java
@@ -175,6 +175,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(List.of("Authorization"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
